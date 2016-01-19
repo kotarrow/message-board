2c1d63bab96409cba306fda02139a10f6b7cdb79
kotarrow <kotarosun@yahoo.co.jp>
2016-01-19 06:57:38 +0000

    update

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,3 @@
-# class MessagesController < ApplicationController
-#   def index
-#   end
-# end
 class MessagesController < ApplicationController
   def index
     @message=Message.new
@@ -11,8 +7,14 @@ class MessagesController < ApplicationController
   
   def create
     @message=Message.new(message_params)
-    @message.save
-    redirect_to root_path , notice:'メッセージを保存しました'
+    if @message.save
+      redirect_to root_path , notice:'メッセージを保存しました'
+    else
+      #バリデーションに失敗し、メッセージが保存できなかった場合
+      @messages = Message.all
+      flash.now[:alert] = "メッセージの保存に失敗しました"
+      render 'index'
+    end
   end
   
   private
